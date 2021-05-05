a336a57356488263fb20cb92ee5e888691daf143
unknown <mlundine@udel.edu>
2021-05-05 13:01:34 -0400

    added clahe, changed visuals

--- Lundine_underwater_color_correction.py
+++ Lundine_underwater_color_correction.py
@@ -10,6 +10,7 @@ import cv2
 import PIL
 import sys
 from PIL import Image
+from PIL.ExifTags import TAGS
 Image.MAX_IMAGE_PIXELS = None
 import skvideo
 import os
@@ -142,6 +143,20 @@ def applyFilter(data, height, width, filterMatrix):
     new_img[new_img>255] = 255
     return new_img.astype('uint8')
 
+def gdal_open(image_path):
+    ### read in image to classify with gdal
+    driverTiff = gdal.GetDriverByName('GTiff')
+    input_raster = gdal.Open(image_path)
+    nbands = input_raster.RasterCount
+    prj = input_raster.GetProjection()
+    gt = input_raster.GetGeoTransform()
+    ### create an empty array, each column of the empty array will hold one band of data from the image
+    ### loop through each band in the image nad add to the data array
+    data = np.empty((input_raster.RasterYSize, input_raster.RasterXSize, nbands))
+    for i in range(1, nbands+1):
+        band = input_raster.GetRasterBand(i).ReadAsArray()
+        data[:, :, i-1] = band
+    return data, prj, gt
 
 ## Base values
 #    MIN_AVG_RED = 60
@@ -149,47 +164,99 @@ def applyFilter(data, height, width, filterMatrix):
 #    BLUE_MAGIC_VALUE = 1.2
 #    Sharpen = 2
 ### Update to run on single, batch, and different parameters
-def single_image(path, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN):
-    img_arr = cv2.imread(path, 1)
-    img_arr = img_arr[:, :, [2, 1, 0]]
-    #exif = img.info['exif']
-    height,width,channels = img_arr.shape
-    avg = calculateAverageColor(img_arr, width, height)
-    filterMatrix = getColorFilterMatrix(img_arr, width, height, avg, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE)
-    filtered_img = applyFilter(img_arr, height, width, filterMatrix)
-    sharp_filt_img = PIL.Image.fromarray(filtered_img)
-    for i in range(SHARPEN):
-        sharp_filt_img = sharp_filt_img.filter(ImageFilter.SHARPEN)
-    
-    sharp_filt_img = cv2.cvtColor(np.array(sharp_filt_img), cv2.COLOR_RGB2BGR)
+def single_image(path, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN, geo=False, clahe=False):
     extension = os.path.splitext(os.path.basename(path))[1]
     new_path = os.path.splitext(os.path.basename(path))[0] + '_cc'
-    
     wd = os.getcwd()
     saveFolder = 'correction_results'
     saveFolder = os.path.join(wd, saveFolder)
     new_path = os.path.join(saveFolder, new_path)
-    if os.path.isdir(saveFolder):
-        time_str = dt.datetime.now().strftime('%Y%m%d%H%M%S')
-        cv2.imwrite(new_path + '_' + time_str + extension, sharp_filt_img)
+    time_str = dt.datetime.now().strftime('%Y%m%d%H%M%S')
+    if geo == False:
+        img_arr = cv2.imread(path, 1)
+        img_arr = img_arr[:, :, [2, 1, 0]]
     else:
-        os.makedirs(saveFolder)
-        time_str = dt.datetime.now().strftime('%Y%m%d%H%M%S')
-        cv2.imwrite(new_path + '_' + time_str + extension, sharp_filt_img)
-    return new_path + '_' + time_str + extension
+        img_arr, prj, gt = gdal_open(path)
+        rasterY, rasterX, nbands = np.shape(img_arr)
+        if nbands >3:
+            alpha = img_arr[:,:,3]
+            img_arr = img_arr[:,:,0:3]
+        print(np.shape(img_arr))
+    height,width,channels = img_arr.shape
+    avg = calculateAverageColor(img_arr, width, height)
+    filterMatrix = getColorFilterMatrix(img_arr, width, height, avg, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE)
+    filtered_img = applyFilter(img_arr, height, width, filterMatrix)
+    if clahe == True:
+        clahefilter = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16,16))
+        #NORMAL
+        # convert to gray
+        gray = cv2.cvtColor(filtered_img, cv2.COLOR_BGR2GRAY)
+        grayimg = gray
+        GLARE_MIN = np.array([0, 0, 50],np.uint8)
+        GLARE_MAX = np.array([0, 0, 225],np.uint8)
+        #CLAHE
+        claheCorrecttedFrame = clahefilter.apply(grayimg)
+
+        #COLOR 
+        lab = cv2.cvtColor(filtered_img, cv2.COLOR_BGR2LAB)
+        lab_planes = cv2.split(lab)
+        clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+        lab_planes[0] = clahe.apply(lab_planes[0])
+        lab = cv2.merge(lab_planes)
+        clahe_bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+        filtered_img = clahe_bgr[:,:,[2,1,0]]
+        save_path = (new_path + '_' + time_str + '_red_' + str(MIN_AVG_RED) +
+             '_hue' + str(MAX_HUE_SHIFT) +
+             '_blue' + str(BLUE_MAGIC_VALUE) +
+             '_sharpen' + str(SHARPEN) + '_clahe' +extension)
+    else:
+        save_path = (new_path + '_' + time_str + '_red_' + str(MIN_AVG_RED) +
+             '_hue' + str(MAX_HUE_SHIFT) +
+             '_blue' + str(BLUE_MAGIC_VALUE) +
+             '_sharpen' + str(SHARPEN) + extension)
+    if SHARPEN > 0:
+        sharp_filt_img = PIL.Image.fromarray(filtered_img)
+        for i in range(SHARPEN):
+            sharp_filt_img = sharp_filt_img.filter(ImageFilter.SHARPEN)
+        sharp_filt_img = cv2.cvtColor(np.array(sharp_filt_img), cv2.COLOR_RGB2BGR)
+    else:
+        sharp_filt_img = filtered_img
+
+    print(save_path)
+    if geo==False:
+        if os.path.isdir(saveFolder):
+            cv2.imwrite(save_path, sharp_filt_img)
+            sharp_filt_img = None
+        else:
+            os.makedirs(saveFolder)
+            cv2.imwrite(save_path, sharp_filt_img)
+            sharp_filt_img = None
+    else:
+        driverTiff = gdal.GetDriverByName('GTiff')
+        clfds = driverTiff.Create(save_path, rasterX, rasterY, nbands, gdal.GDT_Float32)
+        clfds.SetGeoTransform(gt)
+        clfds.SetProjection(prj)
+        for i in range(1,4):
+            clfds.GetRasterBand(i).SetNoDataValue(-9999)
+            clfds.GetRasterBand(i).WriteArray(sharp_filt_img[:,:,i-1])
+        if nbands > 3:
+            clfds.GetRasterBand(4).WriteArray(alpha)
+        clfds = None
+        sharp_filt_img = None  
+    return save_path
      
-def batch_image(folder, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN):
+def batch_image(folder, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN, clahe):
     types = ('/*.jpg', '/*.png', '/*.jpeg', '/*.tif')
     files = []
     for ext in types:
         for im in glob.glob(folder + ext):
             files.append(im)
     for im in files:
-        single_image(im, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN)        
+        single_image(im, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN, clahe=clahe)        
         
-def single_video(vid_path, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN):
-    data = skvideo.io.ffprobe(vid_path)['video']
-    rate = data['@r_frame_rate']
+def single_video(vid_path, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN, clahe):
+    data = skvideo.io.ffprobe(vid_path)
+    rate = data['video']['@avg_frame_rate']
     reader =  skvideo.io.FFmpegReader(vid_path)
     wd = os.getcwd()
     new_vid_path = os.path.splitext(os.path.basename(vid_path))[0]
@@ -207,7 +274,26 @@ def single_video(vid_path, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN
         avg = calculateAverageColor(frame, width, height)
         filterMatrix = getColorFilterMatrix(frame, width, height, avg, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE)
         filtered_img = applyFilter(frame, height, width, filterMatrix)
-        sharp_filt_img = PIL.Image.fromarray(filtered_img)
+        if clahe == True:
+            clahefilter = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16,16))
+            #NORMAL
+            # convert to gray
+            gray = cv2.cvtColor(filtered_img, cv2.COLOR_BGR2GRAY)
+            grayimg = gray
+            GLARE_MIN = np.array([0, 0, 50],np.uint8)
+            GLARE_MAX = np.array([0, 0, 225],np.uint8)
+            #CLAHE
+            claheCorrecttedFrame = clahefilter.apply(grayimg)
+
+            #COLOR 
+            lab = cv2.cvtColor(filtered_img, cv2.COLOR_BGR2LAB)
+            lab_planes = cv2.split(lab)
+            clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+            lab_planes[0] = clahe.apply(lab_planes[0])
+            lab = cv2.merge(lab_planes)
+            clahe_bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+            filtered_img = clahe_bgr[:,:,[2,1,0]]
+            sharp_filt_img = PIL.Image.fromarray(filtered_img)
         for j in range(SHARPEN):
             sharp_filt_img = sharp_filt_img.filter(ImageFilter.SHARPEN)
         final_arr = np.asarray(sharp_filt_img).astype('uint8')
@@ -217,6 +303,26 @@ def single_video(vid_path, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN
 def batch_video(folder, MIN_AVG_RED, MAX_HUE_SHIFT, BLUE_MAGIC_VALUE, SHARPEN):
     for vid in glob.glob(folder + '/*.mp4'):
         single_video(vid)
+
+
+    
+
+def getexif(path):
+    img = Image.open(path)
+    exif = img.getexif()
+    # iterating over all EXIF data fields
+    for tag_id in exif:
+        # get the tag name, instead of human unreadable tag id
+        tag = TAGS.get(tag_id, tag_id)
+        data = exif.get(tag_id)
+        # decode bytes 
+        if isinstance(data, bytes):
+            data = data.decode()
+        print(f"{tag:25}: {data}")
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -40,29 +40,31 @@ class Window(QMainWindow):
 
         
 
-    def correct(self, path, red, hue, blue, sharp, lab, scaleFac):
+    def correct(self, path, red, hue, blue, sharp, clahe, lab, scaleFac):
         new_img = cc.single_image(path, 
                                   red, 
                                   hue, 
                                   blue, 
-                                  sharp)
+                                  sharp,
+                                  clahe=clahe)
         
         pixmap2 = QPixmap(new_img)
         small_pixmap2 = pixmap2.scaled(int(pixmap2.width()/scaleFac), int(pixmap2.height()/scaleFac))
         lab.setPixmap(small_pixmap2)
         lab.resize(int(pixmap2.width()/scaleFac), int(pixmap2.height()/scaleFac))
-        lab.move(960,50)
+        #lab.move(960,50)
+        self.vbox.addWidget(lab, 2, 1)
         lab.show() 
         
     
-    def correct_batch(self, folder, red, hue, blue, sharp):
-        cc.batch_image(folder, red, hue, blue, sharp)
+    def correct_batch(self, folder, red, hue, blue, sharp, clahe):
+        cc.batch_image(folder, red, hue, blue, sharp, clahe)
             
-    def correct_vid(self, file, red, hue, blue, sharp):
-        cc.single_video(file, red, hue, blue, sharp)
+    def correct_vid(self, file, red, hue, blue, sharp, clahe):
+        cc.single_video(file, red, hue, blue, sharp,clahe)
         
         
-    def openFileNameDialog(self, button, imLab, rBut, hBut, bBut, sBut, eBut):
+    def singleImageButton(self, button, imLab, rBut, hBut, bBut, sBut, claheBut, eBut):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         global fileName
@@ -79,13 +81,14 @@ class Window(QMainWindow):
                 logical2 = 50 + (pixmap.height()/scaleFac) >= 790
             small_pixmap = pixmap.scaled(int(pixmap.width()/scaleFac), int(pixmap.height()/scaleFac))
             label.setPixmap(small_pixmap)
-            label.move(130,50)
+            #label.move(130,50)
             label.resize(int(pixmap.width()/scaleFac),int(pixmap.height()/scaleFac))
+            self.vbox.addWidget(label, 1, 1)
             label.show()
 
             button.show()
             button.setEnabled(True)
-            button.clicked.connect(lambda: self.correct(fileName, rBut.value(), hBut.value(), bBut.value(), sBut.value(), imLab, scaleFac))
+            button.clicked.connect(lambda: self.correct(fileName, rBut.value(), hBut.value(), bBut.value(), sBut.value(), claheBut.isChecked(), imLab, scaleFac))
 
             img_buttons = [label, imLab]
             eBut.show()
@@ -93,7 +96,7 @@ class Window(QMainWindow):
             eBut.clicked.connect(lambda: self.exit_func(eBut, button, img_buttons))
             
         
-    def openDirectory(self, button, rBut, hBut, bBut, sBut, eBut):
+    def batchImageButton(self, button, rBut, hBut, bBut, sBut, claheBut, eBut):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         global folderName
@@ -108,9 +111,10 @@ class Window(QMainWindow):
             if len(file_list) < 30:
                 ct = 0
                 for im in file_list:
-                    file_lab = QLabel(im, self)
+                    file_lab = QLabel(im)
                     file_lab.resize(900,20)
                     file_lab.move(500, ct*20)
+                    self.vbox.addWidget(file_lab, 0, ct)
                     file_lab.show()
                     ct = ct+1
                     file_labs.append(file_lab)
@@ -118,6 +122,7 @@ class Window(QMainWindow):
                 file_lab = QLabel('Working on Folder: ' + folderName, self)
                 file_lab.resize(900,20)
                 file_lab.move(500, 0)
+                self.vbox.addWidget(file_lab, 0, 1)
                 file_lab.show()
                 file_labs.append(file_lab)
 
@@ -125,13 +130,13 @@ class Window(QMainWindow):
             button.setEnabled(True)
 
             
-            button.clicked.connect(lambda: self.correct_batch(folderName, rBut.value(), hBut.value(), bBut.value(), sBut.value()))
+            button.clicked.connect(lambda: self.correct_batch(folderName, rBut.value(), hBut.value(), bBut.value(), sBut.value(), claheBut.isChecked()))
 
             eBut.show()
             eBut.setEnabled(True)
             eBut.clicked.connect(lambda: self.exit_func(eBut, button, file_labs))
             
-    def openVideo(self, button, rBut, hBut, bBut, sBut, eBut):
+    def openVideo(self, button, rBut, hBut, bBut, sBut, claheBut, eBut):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         global vidFileName
@@ -145,7 +150,7 @@ class Window(QMainWindow):
 
             button.show()
             button.setEnabled(True)
-            button.clicked.connect(lambda: self.correct_vid(vidFileName, rBut.value(), hBut.value(), bBut.value(), sBut.value()))
+            button.clicked.connect(lambda: self.correct_vid(vidFileName, rBut.value(), hBut.value(), bBut.value(), sBut.value(), claheBut.isChecked()))
 
             videoLabels = [vidLabel]
             eBut.show()
@@ -153,68 +158,88 @@ class Window(QMainWindow):
             eBut.clicked.connect(lambda: self.exit_func(eBut, button, videoLabels))
         
     def home(self):
+        self.scroll = QScrollArea()             # Scroll Area which contains the widgets, set as the centralWidget
+        self.widget = QWidget()                 # Widget that contains the collection of Vertical Box
+        self.vbox = QGridLayout()             # The Vertical Box that contains the Horizontal Boxes of  labels and buttons
+        self.widget.setLayout(self.vbox)
+        
+
         
         ##Button for single image
-        singleImage = QPushButton("Single Image", self)
-        singleImage.resize(100,100)
+        singleImage = QPushButton("Single Image")
+        #singleImage.resize(100,100)
+        self.vbox.addWidget(singleImage, 0, 0)
 
         ## Button for batch of images
-        batchIm = QPushButton("Batch of Images", self)
-        batchIm.resize(100,100)
-        batchIm.move(0,100)
+        batchIm = QPushButton("Batch of Images")
+        #batchIm.resize(100,100)
+        #batchIm.move(0,100)
+        self.vbox.addWidget(batchIm, 1, 0)
         
         ##Button for a single video
-        singleVid = QPushButton('Single Video', self)
-        singleVid.resize(100,100)
-        singleVid.move(0,200)
+        singleVid = QPushButton('Single Video')
+        #singleVid.resize(100,100)
+        #singleVid.move(0,200)
+        self.vbox.addWidget(singleVid,2,0)
         
         ### red slider
-        min_avg_red_label = QLabel('Minimum Average Red', self)
+        min_avg_red_label = QLabel('Minimum Average Red')
         min_avg_red_label.resize(130,25)
         min_avg_red_label.move(5,325)
+        self.vbox.addWidget(min_avg_red_label, 3,0)
         min_avg_red_slider = QSpinBox(self)
         min_avg_red_slider.setMinimum(0)
         min_avg_red_slider.setMaximum(255)
         min_avg_red_slider.setValue(60)
-        min_avg_red_slider.move(5,350)
+        #min_avg_red_slider.move(5,350)
+        self.vbox.addWidget(min_avg_red_slider, 4,0)
         
         
         
         ##hue shift slider
-        max_hue_shift_label = QLabel('Maximum Hue Shift', self)
-        max_hue_shift_label.resize(125,25)
-        max_hue_shift_label.move(5,390)
-        max_hue_shift_slider = QSpinBox(self)
+        max_hue_shift_label = QLabel('Maximum Hue Shift')
+        #max_hue_shift_label.resize(125,25)
+        #max_hue_shift_label.move(5,390)
+        self.vbox.addWidget(max_hue_shift_label, 5,0)
+        max_hue_shift_slider = QSpinBox()
         max_hue_shift_slider.setMinimum(0)
         max_hue_shift_slider.setMaximum(255)
         max_hue_shift_slider.setValue(120)
-        max_hue_shift_slider.move(5,415)
-        
+        #max_hue_shift_slider.move(5,415)
+        self.vbox.addWidget(max_hue_shift_slider, 6,0)
         
         
         #blue magic value slider
-        blue_magic_val_label = QLabel('Blue Magic Value', self)
-        blue_magic_val_label.resize(125,25)
-        blue_magic_val_label.move(5,455)
-        blue_magic_val_slider = QDoubleSpinBox(self)
+        blue_magic_val_label = QLabel('Blue Magic Value')
+        #blue_magic_val_label.resize(125,25)
+        #blue_magic_val_label.move(5,455)
+        self.vbox.addWidget(blue_magic_val_label, 7,0)
+        blue_magic_val_slider = QDoubleSpinBox()
         blue_magic_val_slider.setMinimum(0)
-        blue_magic_val_slider.setMaximum(255)
+        blue_magic_val_slider.setMaximum(5)
         blue_magic_val_slider.setValue(1.2)
-        blue_magic_val_slider.move(5,480)
+        #blue_magic_val_slider.move(5,480)
+        self.vbox.addWidget(blue_magic_val_slider, 8,0)
         
         
         
         ##sharpen slider
-        sharpen_label = QLabel('Sharpen Steps', self)
-        sharpen_label.resize(125,25)
-        sharpen_label.move(5,520)
-        sharpen_slider = QSpinBox(self)
+        sharpen_label = QLabel('Sharpen Steps')
+        #sharpen_label.resize(125,25)
+        #sharpen_label.move(5,520)
+        self.vbox.addWidget(sharpen_label, 9,0)
+        sharpen_slider = QSpinBox()
         sharpen_slider.setValue(1)
-        blue_magic_val_slider.setMinimum(0)
-        blue_magic_val_slider.setMaximum(10)
-        sharpen_slider.move(5,545)
+        sharpen_slider.setMinimum(0)
+        sharpen_slider.setMaximum(4)
+        #sharpen_slider.move(5,545)
+        self.vbox.addWidget(sharpen_slider, 10,0)
         
-        
+        ##clahe checkbox
+        clahe_label = QLabel('CLAHE (adjusts for glare)')
+        clahe = QCheckBox()
+        self.vbox.addWidget(clahe_label,11,0)
+        self.vbox.addWidget(clahe, 12, 0)
         
         ##original image label
         old = QLabel('Original', self)
@@ -229,52 +254,57 @@ class Window(QMainWindow):
         
         
         #run buttons
-        run_algorithm1 = QPushButton('Run', self)
-        run_algorithm1.resize(100,100)
-        run_algorithm1.move(0,600)
+        run_algorithm1 = QPushButton('Run')
+        #run_algorithm1.resize(100,100)
+        #run_algorithm1.move(0,600)
         run_algorithm1.setEnabled(False)
+        self.vbox.addWidget(run_algorithm1, 13,0)
 
-        run_algorithm2 = QPushButton('Run', self)
-        run_algorithm2.resize(100,100)
-        run_algorithm2.move(0,600)
+        run_algorithm2 = QPushButton('Run')
+        #run_algorithm2.resize(100,100)
+        #run_algorithm2.move(0,600)
         run_algorithm2.setEnabled(False)
+        self.vbox.addWidget(run_algorithm2, 13,0)
 
-        run_algorithm3 = QPushButton('Run', self)
-        run_algorithm3.resize(100,100)
-        run_algorithm3.move(0,600)
+        run_algorithm3 = QPushButton('Run')
+        #run_algorithm3.resize(100,100)
+        #run_algorithm3.move(0,600)
         run_algorithm3.setEnabled(False)
+        self.vbox.addWidget(run_algorithm3, 13,0)
         
         #exit button
-        exit_button = QPushButton('Close Images', self)
-        exit_button.resize(100,100)
-        exit_button.move(0,700)
+        exit_button = QPushButton('Close Images')
+        #exit_button.resize(100,100)
+        #exit_button.move(0,700)
         exit_button.setEnabled(False)
-        
+        self.vbox.addWidget(exit_button, 14,0)
         
         ##corrected image label
-        label2 = QLabel(self)
+        label2 = QLabel()
         
         #initialize image path
         image = None
         
         
         ##clicking on single image
-        singleImage.clicked.connect(lambda: self.openFileNameDialog(run_algorithm1,
+        singleImage.clicked.connect(lambda: self.singleImageButton(run_algorithm1,
                                                                     label2,
                                                                     min_avg_red_slider, 
                                                                     max_hue_shift_slider,
                                                                     blue_magic_val_slider,
                                                                     sharpen_slider,
+                                                                    clahe,
                                                                     exit_button
                                                                     ))
 
 
         ##clicking on batch image
-        batchIm.clicked.connect(lambda: self.openDirectory(run_algorithm2,
+        batchIm.clicked.connect(lambda: self.batchImageButton(run_algorithm2,
                                                            min_avg_red_slider, 
                                                            max_hue_shift_slider,
                                                            blue_magic_val_slider,
                                                            sharpen_slider,
+                                                           clahe,
                                                            exit_button
                                                            ))
         
@@ -284,11 +314,17 @@ class Window(QMainWindow):
                                                          max_hue_shift_slider,
                                                          blue_magic_val_slider,
                                                          sharpen_slider,
+                                                         clahe,
                                                          exit_button))
         
         
         
-        self.show()
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setWidget(self.widget)
+
+        self.setCentralWidget(self.scroll)
         run_algorithm1.hide()
         run_algorithm2.hide()
         run_algorithm3.hide()
@@ -297,6 +333,7 @@ class Window(QMainWindow):
 def run():
     app = QApplication(sys.argv)
     GUI = Window()
+    GUI.show()
     sys.exit(app.exec_())
 
 run()
